911522fadf1e01f0c99f7aee530036e7567aafaa
Vlad Fedenyov <fedenev.vlad41@gmail.com>
2024-04-09 00:01:54 +0300

    Added button Publish.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     path('^post/new/$', views.post_new, name='post_new'),
     path('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
